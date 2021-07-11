87f03e8478d54565a0047b83544db367615f1b04
kikerless <2012kiker2012@gmail.com>
2021-07-11 14:46:44 +0300

    add working assert

--- test_items.py
+++ test_items.py
@@ -1,6 +1,9 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_language(browser):
+def test_basket_button_all_languages(browser):
     browser.get(link)
-    browser.find_element_by_class_name("btn-add-to-basket")
+    # x = browser.find_element_by_class_name("btn-add-to-basket")
+    # assert x is not None, 'СООБЩЕНИЕ'
+    assert "btn-add-to-basket1" in browser.page_source, 'СООБЩЕНИЕ'
+
