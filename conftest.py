6dacdec939d5619d1a7c4ffdfb562600038cb587
kikerless <2012kiker2012@gmail.com>
2021-07-11 13:32:42 +0300

    start browser with True language

--- conftest.py
+++ conftest.py
@@ -3,6 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose browser language")
+
+
 @pytest.fixture(scope="function")
 def browser(request):
     print("\nStart browser for test..")
